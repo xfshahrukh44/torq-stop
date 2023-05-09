77911384432cd1de49a91fec4665131392ba3c95
xfshahrukh <ssidduit@gmail.com>
2023-05-09 14:07:47 +0500

    contact us dynamic

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -26,7 +26,7 @@ class FrontController extends Controller
 //            $email = (Settings::find(1))->email;
             $email = 'no-reply@torqstop.com';
 
-            send_mail('no-reply@torqstop.com', $email, $subject, $message);
+            send_mail('no-reply@torqstop.com', $email, 'Customer Contact', $message);
 
             return redirect()->route('shop.home.index')->with('success', 'Email sent successfully!');
         } catch (\Exception $exception) {
