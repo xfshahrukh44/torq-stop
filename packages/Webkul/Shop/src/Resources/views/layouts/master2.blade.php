afa6bfc88285b523825c083ab12725439f328351
xfshahrukh <ssidduit@gmail.com>
2023-05-09 17:01:47 +0500

    state names on modal

--- packages/Webkul/Shop/src/Resources/views/layouts/master2.blade.php
+++ packages/Webkul/Shop/src/Resources/views/layouts/master2.blade.php
@@ -39,7 +39,7 @@
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Vendors Across States</h5>
+                    <h5 class="modal-title" id="exampleModalLongTitle2">Vendors Across States</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
