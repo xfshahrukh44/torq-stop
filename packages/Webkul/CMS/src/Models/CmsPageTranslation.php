c6b9326d0335f0131bb751d950dee256157cf973
talal.fazal <talal.fazal@olivedigital.com>
2023-05-10 01:21:53 +0500

    chngess

--- packages/Webkul/CMS/src/Models/CmsPageTranslation.php
+++ packages/Webkul/CMS/src/Models/CmsPageTranslation.php
@@ -3,13 +3,13 @@
 namespace Webkul\CMS\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Spatie\MediaLibrary\HasMedia;
+//use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Webkul\CMS\Contracts\CmsPageTranslation as CmsPageTranslationContract;
 
-class CmsPageTranslation extends Model implements CmsPageTranslationContract, HasMedia
+class CmsPageTranslation extends Model implements CmsPageTranslationContract
 {
-    use InteractsWithMedia;
+//    use InteractsWithMedia;
     public $timestamps = false;
 
     protected $fillable = [
